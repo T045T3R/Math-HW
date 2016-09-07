41c72de484b094ee3c25b602682d7013b28074c4
T045T3R <johannestestorf@hanovernorwichschools.org>
2016-09-07 12:59:25 -0400

    Updated from Brython Server: 9/7/2016 12:59:24 PM

--- helloworld.py
+++ helloworld.py
@@ -1,7 +1,7 @@
 """
 helloworld.py
-Author: <your name here>
-Credit: <list sources used, if any>
+Author: <Johannes Testorf>
+Credit: <none>
 
 Assignment:
 
@@ -9,4 +9,4 @@ Write and submit a Python program that prints the following:
 
 Hello, world!
 """
-
+print ("Hello, world!")
